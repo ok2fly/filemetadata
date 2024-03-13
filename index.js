9deeb21e560c935beb1f1a0b612a22f473360c62
dbafu <2899109958@qq.com>
2024-03-13 14:22:07 +0800

    feat: ✨update

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+	res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+	// debugger;
+	const file = req.file;
+
+	console.log(file);
+
+	// debugger;
+	res.json({
+		name: file.originalname,
+		type: file.mimetype,
+		size: file.size,
+	});
 });
 
 
